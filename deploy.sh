373f6c1de888d8b3f6ee14fd82627a3f777ef929
Santiago-Penaranda-Peinado <santielpilo783@gmail.com>
2025-10-09 16:08:01 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,10 @@
 
 echo "--- Iniciando despliegue en $(date) ---"
 
-# Navegamos a la carpeta raíz del repositorio. 
-# Asegúrate de que esta ruta sea la correcta para tu servidor.
-cd /var/www/vhosts/ecoservicemexiquense.com.mx/Eco-Service-Landingpage
+# Navegamos a la carpeta httpdocs donde está el package.json
+cd /var/www/vhosts/ecoservicemexiquense.com.mx/httpdocs
 
 # Cargamos el entorno específico de Node.js v24 para tener acceso a 'npm'
-# Esta línea es la clave para que encuentre los comandos.
 source /opt/plesk/node/24/enable
 
 # Ejecutamos los comandos de compilación
